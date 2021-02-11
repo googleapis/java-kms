064787d3562ec22ae132dac3d9d849a66943798c
yoshi-automation <yoshi-automation@google.com>
2021-02-11 15:32:02 -0800

    chore: update gapic-generator-java to 0.0.18
    Committer: @miraleung
    PiperOrigin-RevId: 350949863
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Sat Jan 9 14:01:49 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 91e206bcfeaf8948ea03fe3cb1b7616108496cd3
    Source-Link: https://github.com/googleapis/googleapis/commit/91e206bcfeaf8948ea03fe3cb1b7616108496cd3

--- google-cloud-kms/src/main/java/com/google/cloud/kms/v1/stub/KeyManagementServiceStub.java
+++ google-cloud-kms/src/main/java/com/google/cloud/kms/v1/stub/KeyManagementServiceStub.java
@@ -67,7 +67,7 @@ import javax.annotation.Generated;
  *
  * <p>This class is for advanced usage and reflects the underlying API directly.
  */
-@Generated("by gapic-generator")
+@Generated("by gapic-generator-java")
 public abstract class KeyManagementServiceStub implements BackgroundResource {
 
   public UnaryCallable<ListKeyRingsRequest, ListKeyRingsPagedResponse> listKeyRingsPagedCallable() {
